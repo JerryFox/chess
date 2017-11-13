aee8c0f71c66d168cbcb2317c1f08185f8ee2d81
Jaroslav Vysoky <jaroslav.vysoky@gmail.com>
2017-11-13 21:25:21 +0000

    bottle app for chess

---
+++ bottle_app.py
@@ -0,0 +1,68 @@
+# files serving with directory list
+# simple bottle application
+
+from bottle import default_app, route, static_file
+import os, os.path
+
+ROOT = "/home/vysoky/seminar"   # where are files serving from
+PATH_PREFIX = "/files"          # path prefix in browser
+
+@route("/")
+def kudy_z_nudy():
+    return "Kudy z nudy..."
+
+@route("/chessboard")
+@route("/chessboard/")
+@route("/chessboard/<position>")
+def chessboard(position="base"):
+    import chess
+    chess.CHESS_IMG_FOLDER = "/files/chess/images/"
+    ch = chess.Chessboard(position)
+    return ch.html()
+
+@route(PATH_PREFIX)
+@route(PATH_PREFIX + '<filepath:path>')
+def server_static(filepath="/"):
+    ipath = ROOT + filepath
+    if os.path.isdir(ipath):
+        list_dir = os.listdir(ipath)
+        html_template = """
+<html>
+    <head>
+        <title>List of {path}</title>
+        <style>
+            li.file {{
+                list-style-type: circle;
+            }}
+            li.folder {{
+                list-style-type: square;
+            }}
+        </style>
+    </head>
+    <body>
+        <h2>Content of {path}</h2>
+        <ul>
+{items}
+        </ul>
+    </body>
+</html>
+"""
+        list_isfile = []
+        # list of (<isfile>, <name>)
+        for i in list_dir:
+            list_isfile.append((os.path.isfile(os.path.join(ipath,i)),i))
+        # sort - folders first then sorted by names
+        list_isfile.sort(key = lambda x: str(x[0]) + x[1])
+        items = ""
+        for item in list_isfile:
+            iclass = "file" if item[0] else "folder"
+            line = '<li class="{}"><a href="{}">{}</a></li>\n'
+            items += line.format(iclass,PATH_PREFIX + os.path.join(filepath, item[1]), item[1])
+        return html_template.format(path=filepath, items=items)
+    else:
+        return static_file(filepath, root=ROOT)
+
+
+
+application = default_app()
+
