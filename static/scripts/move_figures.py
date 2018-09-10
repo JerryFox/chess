7ad9711d13fb7c1fbf27d42ac9c41e9356acf6e2
Vysoký Jaroslav <vysokyjar@kraken.pedf.cuni.cz>
2018-09-10 22:55:35 +0200

    moving figures / pictures - example

---
+++ static/scripts/move_figures.py
@@ -0,0 +1,69 @@
+# move all figures to the right
+
+from browser import document as doc, timer
+
+b = doc.ch_board
+
+def go_board(self): 
+    figures = []
+    for row in self.figures: 
+        for col in row: 
+            for fig in col: 
+                figures.append(fig)
+    rest = False
+    for fig in figures: 
+        if fig.col == 7: 
+            fig.remove()
+        else: 
+            fig.move_to(fig.row, fig.col + 1)
+            rest = True
+    if rest:
+        self.timer_interval = 1000
+        self.idtimer = timer.set_timeout(self.go, self.timer_interval)
+
+def shift_images(self):
+    figures = []
+    for row in self.figures: 
+        for col in row: 
+            for fig in col: 
+                figures.append(fig)
+    dshiftx = 20
+    dshifty = -10
+    self.shift[0] += dshiftx
+    self.shift[1] += dshifty
+    for fig in figures: 
+        x = int(fig.svg_image.getAttribute("x"))
+        fig.svg_image.setAttribute("x", str(x + dshiftx))
+        y = int(fig.svg_image.getAttribute("y"))
+        fig.svg_image.setAttribute("y", str(y + dshifty))
+    if self.shift[0] < 1000:
+        self.timer_interval = 10
+        self.idtimer = timer.set_timeout(self.sh, self.timer_interval)
+        
+def reshift_images(self):
+    figures = []
+    for row in self.figures: 
+        for col in row: 
+            for fig in col: 
+                figures.append(fig)
+    for fig in figures: 
+        x = int(fig.svg_image.getAttribute("x"))
+        y = int(fig.svg_image.getAttribute("y"))
+        fig.svg_image.setAttribute("x", str(x - self.shift[0]))
+        fig.svg_image.setAttribute("y", str(y - self.shift[1]))
+    self.shift[0] = 0
+    self.shift[1] = 0
+
+def bind(instance, method):
+    def binding_scope_fn(*args, **kwargs): 
+        return method(instance, *args, **kwargs)
+    return binding_scope_fn
+
+b.go = bind(b, go_board)
+#b.go()
+b.shift = [0, 0]
+b.sh = bind(b, shift_images)
+b.resh = bind(b, reshift_images)
+
+
+
